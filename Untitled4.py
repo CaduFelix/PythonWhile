ea6cdabb4ea58770e9fbfc2ab5ea3d48f2b47cd1
CaduFelix <50344506+CaduFelix@users.noreply.github.com>
2021-11-10 13:05:49 -0300

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+contador = 0 
+
+while contador < 10:
+    contador = contador + 1
+    if contador == 1 :
+        print (contador, "item limpo")
+    else:
+        print (contador, "itens limpos")
+        
+
+        
+    
+            
+
+
+# In[ ]:
+
+
+
+
